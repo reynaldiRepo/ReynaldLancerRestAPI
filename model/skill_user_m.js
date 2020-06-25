fc7bd82fd4349e3769dc5ad4b08d5ffbceb161e6
Reynaldi <38524172+reynaldiRepo@users.noreply.github.com>
2020-06-25 21:28:21 +0700

    fixing model

--- model/skill_user_m.js
+++ model/skill_user_m.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const skill = new schema({
+    "_id":{
+        type:String
+    },
     "user_id":{
         type:String
     },
